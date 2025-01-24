98863da4477fc7fed056bc6254b68f081a61d400
Mroziu0 <mroziu86@gmail.com>
2025-01-24 12:14:05 +0000

    poc

---
+++ raspdemo/src/main/java/com/rasp/example/raspdemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.rasp.example.raspdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
---
+++ raspdemo/src/main/java/com/rasp/example/raspdemo/RaspdemoApplication.java
@@ -0,0 +1,13 @@
+package com.rasp.example.raspdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspdemoApplication.class, args);
+	}
+
+}
